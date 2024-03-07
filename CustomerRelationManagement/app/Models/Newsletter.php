d65d914ff2daa63257cd8efbb68085b04da5add7
CrimSonRice <92437794+CrimSonRice@users.noreply.github.com>
2024-03-07 14:55:29 +0700

    Udpated msft

---
+++ CustomerRelationManagement/app/Models/Newsletter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FtSda3\CustomerRelationManagement\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Newsletter extends Model
+{
+    use HasFactory;
+}
